3825d245d968ec1d2c7703c037031a8fc27db4e2
samir-shakya <samir.shakya2024@gmail.com>
2025-04-01 18:33:18 +0545

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let typed = new Typed('#element', {
+  strings: ['Technical Support Officer'],
+  typeSpeed: 50,
+  loop: true,
+});
+window.addEventListener('DOMContentLoaded' , () => {
+  VANTA.BIRDS({
+      el: "#animation",
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 200.00,
+      minWidth: 200.00,
+      scale: 1.00,
+      scaleMobile: 1.00,
+      backgroundColor: 0x1a202c
+  })
+})
